8ee21de81c86a83519320334117da98d0480a0fe
mauri <morocobt@gmail.com>
2023-09-22 00:34:03 -0400

    add random word using open ai api

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-var keyboard = document.querySelector("#keyboard");
+const API_KEY = data.key;
+const word = document.querySelector("h2");
 
-for (var i = "A".charCodeAt(0); i <= "Z".charCodeAt(0); i++) {
-    var key = document.createElement("button");
+async function getCompletion() {
+    const response = await fetch(`https://api.openai.com/v1/completions`, {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+        },
+        body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt: "A random easy noun. No punctuation. All letters capitalized.",
+        max_tokens: 100,
+        }),
+    });
+
+    const data = await response.json();
+    word.innerHTML = data.choices[0].text;
+}
+
+getCompletion();
+
+const keyboard = document.querySelector("#keyboard");
+
+for (let i = "A".charCodeAt(0); i <= "Z".charCodeAt(0); i++) {
+    const key = document.createElement("button");
     key.innerHTML = String.fromCharCode(i);
 
     key.addEventListener("click", function() {
