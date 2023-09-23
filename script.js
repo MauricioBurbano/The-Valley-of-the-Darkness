289e9f27641b47d364ba0762efa21503733d9d55
mauri <morocobt@gmail.com>
2023-09-23 00:28:20 -0400

    add hidden word and guess functionality

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const API_KEY = data.key;
 const word = document.querySelector("h2");
+let randomWord = "";
+let wordArr;
+let guess;
 
 async function getCompletion() {
     const response = await fetch(`https://api.openai.com/v1/completions`, {
@@ -10,13 +13,21 @@ async function getCompletion() {
         },
         body: JSON.stringify({
         model: "text-davinci-003",
-        prompt: "A random easy noun. No punctuation. All letters capitalized.",
+        prompt: "A random easy to guess noun. No punctuation. All letters capitalized.",
         max_tokens: 100,
         }),
     });
 
     const data = await response.json();
-    word.innerHTML = data.choices[0].text;
+    
+    for (let i = 2; i < data.choices[0].text.length; i++) {
+        randomWord += data.choices[0].text[i];
+    }
+
+    wordArr = new Array(randomWord.length).fill("__");
+    guess = wordArr.join(" ");
+
+    word.innerHTML = guess + " " + randomWord;
 }
 
 getCompletion();
@@ -28,12 +39,23 @@ for (let i = "A".charCodeAt(0); i <= "Z".charCodeAt(0); i++) {
     key.innerHTML = String.fromCharCode(i);
 
     key.addEventListener("click", function() {
-        console.log(this.innerHTML);
+        check(this.innerHTML);
     });
 
     keyboard.appendChild(key);
 }
 
 document.addEventListener("keydown", function(event) {
-    console.log(event.key.toUpperCase());     
+    check(event.key.toUpperCase());
 });
+
+function check(letter) {
+    for (let i = 0; i < randomWord.length; i++) {
+        if (letter === randomWord[i]) {
+            wordArr[i] = letter;
+        }
+
+        guess = wordArr.join(" ");
+        word.innerHTML = guess;
+    }
+}
